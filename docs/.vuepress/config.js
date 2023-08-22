873ec19ff0609a236f1e3dc474f3c90dc3bf94af
连夜扛火车跑路 <1218187864@qq.com>
2023-08-22 08:15:10 +0800

    部分内容增加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,10 @@ module.exports = (options, context) => {
 	return {
 		base: '/quweiqian/',
 		title: '无间劫',
+		head:[
+			['link',{rel:'stylesheet',href:'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'}],
+			['link',{rel:'stylesheet',href:'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css'}],
+		],
 		description: 'Just playing around',
 		
 		plugins: [
@@ -53,6 +57,10 @@ module.exports = (options, context) => {
 				}
 			}
 		},
+		extendMarkdown(md){
+			md.set({html:true});
+			md.use(require("markdown-it-katex"));
+		}
 	}
 }
 
