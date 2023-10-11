0d8cd514ffed5878c855992b9e1473f372aeb6af
连夜扛火车跑路 <1218187864@qq.com>
2023-10-11 21:31:46 +0800

    deploy

--- docs/.vuepress/configs/particular/html5.js
+++ docs/.vuepress/configs/particular/html5.js
@@ -4,5 +4,15 @@ module.exports = {
   sidebarDepth: 2,    // 可选的, 默认值是 1
   children: [
     "html5/1",
+    {
+      title: 'svg详细版',   // 必要的
+      collapsable: true, // 可选的, 默认值是 true,
+      sidebarDepth: 2,    // 可选的, 默认值是 1
+      children: [
+        "html5/2-1",
+        "html5/2-2",
+      ]
+    },
+    "html5/3",
   ]
 }
