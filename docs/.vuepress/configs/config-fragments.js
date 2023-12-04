395b6e18582fa9013e475ab908911150bb2c9a2e
连夜扛火车跑路 <1218187864@qq.com>
2023-12-04 20:41:22 +0800

    deploy

--- docs/.vuepress/configs/config-fragments.js
+++ docs/.vuepress/configs/config-fragments.js
@@ -10,6 +10,22 @@ module.exports = {
         "interview-camp/3",
       ]
     },
+    {
+      title: '9j网络',   // 必要的
+      collapsable: true, // 可选的, 默认值是 true,
+      sidebarDepth: 2,    // 可选的, 默认值是 1
+      children: [
+        '9j/1为什么学网络',
+        '9j/2网络传输过程',
+        '9j/3内网IP和外网IP',
+        '9j/7或运算和与运算',
+        '9j/8子网掩码',
+        '9j/9物理层',
+        '9j/10数据链路层',
+        '9j/11网络层',
+        '9j/12IP数据报'
+      ]
+    },
     "1",
     "2",
     "3",
